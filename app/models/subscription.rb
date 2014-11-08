606c855a4b575ef9981bf5d397c71f3209c66f91
Ethan Dennis <erdennis13@gmail.com>
2014-11-08 14:33:57 -0800

    Dependant: :destroy for history and subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,6 @@
 class Subscription < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :workout
-	has_many :histories
+	has_many :histories, dependent: :destroy
 	validates_uniqueness_of :workout_id, scope: :user_id
 end
