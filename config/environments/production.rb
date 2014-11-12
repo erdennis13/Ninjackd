ef960c982e6e06d7ab5f57818fc3afebedb3cf25
Ethan Dennis <erdennis13@gmail.com>
2014-11-12 12:59:28 -0800

    Working to get heroku mailer working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { :host => 'http://www.simplefitapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://nameless-headland-4616.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
 
