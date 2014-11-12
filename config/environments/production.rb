88e4c405aa3b29916aa6278235b9abe374ad79ab
Ethan Dennis <erdennis13@gmail.com>
2014-11-12 14:09:08 -0800

    still staging production.rb for heroku mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,12 +86,12 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
+  address: "smtp.simplefitapp.com",
   port: 587,
   domain: 'simplefitapp.com',
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: 'erdennis13',
-  password: 'michtrack1204'
+  user_name: 'ethan',
+  password: 'MICHtrack13!'
   }
 end
