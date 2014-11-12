d1becddf4349ae6afdcdf8d864c537bec26ce2fe
Ethan Dennis <erdennis13@gmail.com>
2014-11-12 13:36:39 -0800

    Staging production file for heroku mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,12 +76,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  Rails.application.routes.default_url_options[:host] = 'www.simplefitapp.com'
 
   config.action_mailer.default_url_options = { :host => 'http://www.simplefitapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
