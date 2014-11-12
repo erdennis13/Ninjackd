01ba5ac55b7149e5dd91e951e0a3b622f6b29c8a
Ethan Dennis <erdennis13@gmail.com>
2014-11-12 12:23:36 -0800

    Added conditions for validated uniqueness of subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,6 +2,8 @@ class Subscription < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :workout
 	has_many :histories, dependent: :destroy
-	validates_uniqueness_of :workout_id, scope: :user_id
+
+	#This only allows users to be subscribed to unique workouts, however, once they complete them, they can subscribe again
+	validates_uniqueness_of :workout_id, scope: :user_id, conditions: -> { where(complete: false) }
 	validates_presence_of :user_id, :workout_id
 end
