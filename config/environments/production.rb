8895088a408ff404594ad7e14a8607a993928609
Ethan Dennis <erdennis13@gmail.com>
2014-11-14 15:46:18 -0800

    Added smtp settings in google apps

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
+  address: "smtp-relay.gmail.com",
   port: 587,
   domain: 'ninjackd.com',
   authentication: "login",
