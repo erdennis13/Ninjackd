f3cbf5432bd9c7b5307436d35d32fe2ae2d1b59e
Ethan Dennis <erdennis13@gmail.com>
2014-11-18 18:25:49 -0800

    Filtered subscriptions to only show future

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -25,7 +25,7 @@ class SubscriptionsController < ApplicationController
   end
 
   def profile
-    @subscriptions = current_user.subscriptions.where(:complete => false).where.not(schedule: Date.today).order("schedule ASC").paginate(:page => params[:page], :per_page => 10)
+    @subscriptions = current_user.subscriptions.where(:complete => false, schedule: Date.today..Date.today + 14).where.not(schedule: Date.today).order("schedule ASC").paginate(:page => params[:page], :per_page => 10)
     @scheduled = current_user.subscriptions.where.not(schedule: nil )
     today = Date.today
     @days_from_this_week = (today.at_beginning_of_week(:sunday)..today.at_end_of_week(:sunday)).map
