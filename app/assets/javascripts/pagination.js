87b60e1af9d310d90dc515b58f18d452f95c3486
Ethan Dennis <erdennis13@gmail.com>
2014-12-13 17:07:46 -0800

    Corrected pagination ajax functionality to only run when I fucking want it to

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,5 +1,6 @@
 $(function() {
-	$(document).delegate(".pagination a", "click", function() {
+//	$("#homepage").delegate(".pagination a", "click", function() {
+	$(document).on("click", "#homepage .pagination a", function() {
 		$(".pagination").html("Page is loading...")
 		$.get(this.href, null, null, "script");
 		history.pushState(null, document.title, this.href )
@@ -8,4 +9,4 @@ $(function() {
 	$(window).bind("popstate", function() {
 		$.getScript(location.href);
 	});
-})
+});
