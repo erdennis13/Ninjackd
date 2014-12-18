9e40ce0567d376df58287094e57cc34862365fdb
Ethan Dennis <erdennis13@gmail.com>
2014-12-17 17:34:20 -0800

    Added loading icon for ajax requests

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,17 +1,26 @@
 $(function() {
-//	$("#homepage").delegate(".pagination a", "click", function() {
 	$(document).on("click", "#homepage .pagination a", function() {
-		$(".pagination").html("Page is loading...")
-		$.get(this.href, null, null, "script");
-		history.pushState(null, document.title, this.href )
+		//$(".pagination").show("#loading-icon")
+		$.get(this.href, null, null, "script")
+		$(".pagination").hide("#loading-icon");
+		//history.pushState(null, document.title, this.href )
 		return false;
 	});
 	$(document).on("click", "#showpage .pagination a", function() {
-		$(".pagination").html("Whats up motherfucker...")
-		$.get(this.href , null, null, "script");
+		$(".pagination").html()
+		$.get(this.href , null, null, "script")
+		$(".pagination").hide("#loading-icon");;
 		return false;
 	});
 	$(window).bind("popstate", function() {
 		$.getScript(location.href);
 	});
+	$('#loading-icon').hide();  // hide it initially.
+  $(document)  
+    .ajaxStart(function() {
+      $('#loading-icon').show(); // show on any Ajax event.
+    })
+    .ajaxStop(function() {
+      $('#loading-icon').hide(); // hide it when it is done.
+  });
 });
