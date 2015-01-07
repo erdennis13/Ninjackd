20cc962a8167f5ed9a7b01548a17045f3cb23036
Ethan Dennis <erdennis13@gmail.com>
2015-01-06 17:59:31 -0800

    smpt fucking sucks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,7 @@ Rails.application.configure do
   domain: 'ninjackd.com',
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: 'ninjackd',
+  user_name: 'ninjackd@gmail.com',
   password: 'michtrack13'
  # user_name: ENV["GMAIL_USERNAME"],
  # password: ENV["GMAIL_PASSWORD"]
