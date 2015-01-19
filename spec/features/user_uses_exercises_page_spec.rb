4de7a43d2e902d594f80ce3219baa26dacb6b8b6
Ethan Dennis <erdennis13@gmail.com>
2015-01-19 13:07:18 -0800

    Feature spec for user uses exercises page mobile version

--- spec/features/user_uses_exercises_page_spec.rb
+++ spec/features/user_uses_exercises_page_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 
 #This feature is for the desktop version.
-feature "User visits exercises page" do
+feature "User visits desktop exercises page" do
 	scenario "and sees exercise list" do
 		visit_exercises_page
 		expect(page).to have_css "table th", text: "Description"
@@ -37,3 +37,36 @@ feature "User visits exercises page" do
 end
 
 #This feature will be for the mobile version.
+feature "User visits mobile exercises page" do
+	scenario "and sees exercise list" do
+		visit_exercises_page
+		expect(page).to have_css "table th", text: "Description"
+	end
+
+	scenario "and searches for exercise" do
+		exercise = Exercise.new(name: "TestEx", description: "Test Description")
+		exercise2 = Exercise.new(name: "TestTwo", description: "Second Description")
+		exercise.save!
+		exercise2.save!
+		visit_exercises_page
+
+		expect(page).to have_css "table tr td", text: "TestEx"
+		expect(page).to have_css "table tr td", text: "TestTwo"
+
+		fill_in "search_mobile_field", with: "TestEx"
+		click_on "filter_mobile_button"
+
+		expect(page).to have_css "table tr td", text: "TestEx"
+		expect(page).not_to have_css "table tr td", text: "TestTwo"
+
+		click_on "reset_mobile_filter_button"
+
+		expect(page).to have_css "table tr td", text: "TestEx"
+		expect(page).to have_css "table tr td", text: "TestTwo"
+	end
+
+	def visit_exercises_page
+		sign_user_in
+		click_on "Exercises"
+	end
+end
