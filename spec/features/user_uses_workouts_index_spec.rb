fc603c382b4336d6bcced65ee1b979b40f84014f
Ethan Dennis <erdennis13@gmail.com>
2015-01-20 08:25:28 -0800

    working on feature tests for using workouts#index page

---
+++ spec/features/user_uses_workouts_index_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+feature "User visits workouts#index" do
+	before(:all) do
+    sign_user_in
+  end
+
+	scenario "and sees workouts listed" do
+		add_one_workout
+		visit workouts_path
+
+		expect(page).to have_css "table tr td", text: "Test strength workout"
+	end
+
+	scenario "and searches for workouts" do
+		add_two_workouts
+		visit workouts_path
+		#Searches by name
+		fill_in "workouts_search_field", with: "TestOne"
+		click_button "filter_button"
+
+		expect(page).to have_css "table tr td", text: "TestOne"
+		expect(page).not_to have_css "table tr td", text: "TestTwo"
+	end
+
+	def add_one_workout
+		w1 = Workout.new(name: "TestOne", category: "Strength", description: "Test strength workout", 
+			duration: 60, weightFacility: nil, active: true, note: "Testing note for TestOne")
+		w1.save!
+	end
+
+	def add_two_workouts
+		w1 = Workout.new(name: "TestOne", category: "Strength", description: "Test strength workout", 
+			duration: 60, weightFacility: nil, active: true, note: "Testing note for TestOne")
+		w2 = Workout.new(name: "TestTwo", category: "Cardio", description: "Test cardio workout", 
+			duration: 45, weightFacility: nil, active: true, note: "Testing note for TestTwo")
+		w1.save!
+		w2.save!
+	end
+end
