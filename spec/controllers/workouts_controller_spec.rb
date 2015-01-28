62ebae48c5078a7e6ed6fd303672af897a539185
Ethan Dennis <erdennis13@gmail.com>
2015-01-27 17:57:49 -0800

    Controller test for workouts rendering views with search params

--- spec/controllers/workouts_controller_spec.rb
+++ spec/controllers/workouts_controller_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 describe WorkoutsController do
+	render_views
 	describe "#index" do
 		it "renders workout list" do
 
@@ -9,5 +10,20 @@ describe WorkoutsController do
 
 			expect(response).to render_template "index"
 		end
+
+		it "searches by scopes" do
+			Workout.create(name: "TestOne", category: "Strength", description: "Test strength workout", 
+			duration: 60, weightFacility: nil, active: true, note: "Testing note for TestOne")
+			user = User.create(name: "User", username: "Username", admin: false, paypal_payment_token: "Test", 
+      email: "user@example.com", password: "password")
+
+			sign_in user
+
+			get :index, search: "Test", less_than: 75, cat: "Strength"
+
+			#expect(response).to have_css "table tr td", text: "TestOne"
+			expect(response).to render_template "index"
+		end
+
 	end
 end
