1dc7261a3014aa77fb2f3061fbdb6408b6d2c800
Ethan Dennis <erdennis13@gmail.com>
2015-02-10 07:29:34 -0800

    Comments controller spec for deleting comment

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,25 +3,48 @@ require "rails_helper"
 describe CommentsController do
 	describe "#create" do
 		before(:each) do
+			@request.env['HTTP_REFERER'] = '/workouts/1'
 			@workout = create(:workout)
 			@user = create(:user)
 			sign_in @user
 		end
+
 		it "makes a regular comment" do
-			@request.env['HTTP_REFERER'] = '/workouts/1'
-			post :create, comment: set_up_creation("This is a test comment")
+			post :create, comment: attributes_for(:comment)
 
 			expect(response).to redirect_to workout_path @workout
 		end
 		it "requires text in comment field" do
-			@request.env['HTTP_REFERER'] = '/workouts/1'
 			post :create, comment: {user_id: @user.id, workout_id: @workout.id, comment: nil}
 
 			expect(response).to redirect_to workout_path @workout
 		end
 	end
+
+	describe "Delete destroy" do
+
+		before(:each) do
+			@request.env['HTTP_REFERER'] = '/workouts/1'
+			@workout = create(:workout)
+			@user = create(:user)
+			sign_in @user
+			@comment = create(:comment, workout_id: @workout.id, user_id: @user.id)
+		end
+
+		it "deletes the comment" do
+			expect{ delete :destroy, id: @comment }.to change(Comment,:count).by(-1)
+		end
+
+		it "redirects to workout page" do
+			delete :destroy, id: @comment
+
+			expect(response).to redirect_to workout_path @workout
+		end
+	end
+
 	def set_up_creation comment
 
 		attributes = {user_id: @user.id, workout_id: @workout.id, comment: comment}
 	end
+
 end
