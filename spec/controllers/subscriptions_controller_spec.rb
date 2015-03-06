7295c5ebd917062f3ad3826776c867bdd56b4563
Ethan Dennis <erdennis13@gmail.com>
2015-03-05 16:29:57 -0800

    Filling out subscriptions controller spec

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -1,6 +1,32 @@
 require "rails_helper"
 
 describe SubscriptionsController do
+	describe "#index" do
+		it "renders index template" do
+			get :index
+
+			expect(response).to render_template :index
+		end
+	end
+
+	describe "#show" do
+		it "renders show template" do
+			subscription = create(:subscription)
+
+			get :show, id: subscription
+
+			expect(response).to render_template :show
+		end
+	end
+
+	describe "#new" do
+		it "creates a new subscription with attributes" do
+			get :new
+
+			expect(assigns(:subscription)).to be_a_new(Subscription)
+		end
+	end
+
 	describe "#create" do
 		it "makes a run of the mill subscription" do
 			post :create, subscription: set_up_creation(Date.today)
